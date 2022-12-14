653a581ce540256072d738f89ea0db5b1c16ee92
pauCM-es <costamingarro@gmail.com>
2022-12-14 21:54:56 +0100

    Iteration#3

--- index.js
+++ index.js
@@ -36,3 +36,25 @@ car1.finalPrice = car1.basePrice + globalBasePrice
 car2.finalPrice = car2.basePrice + globalBasePrice
 console.log(car1, car2);
 
+//? Iteracion #3: Operadores
+
+// 1.1
+console.log(10 * 5)
+
+// 1.2
+console.log(10 / 2)
+
+// 1.3
+console.log(15%9)
+
+// 1.4
+y = 10
+z = 5
+x = y + z
+console.log(x)
+
+// 1.5
+
+x = y * z
+console.log(x)
+
